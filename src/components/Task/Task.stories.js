a8ed851a83f8e42b31c8313c590f9214290612c7
Soni Kumari <soniisharwal@gmail.com>
2021-03-24 16:31:45 +0530

    updated title

--- src/components/Task/Task.stories.js
+++ src/components/Task/Task.stories.js
@@ -13,7 +13,7 @@ export const Default = Template.bind({});
 Default.args = {
   task: {
     id: "1",
-    title: "Test Task",
+    title: "Test Task.",
     state: "TASK_INBOX",
     updatedAt: new Date(2018, 0, 1, 9, 0),
   },
